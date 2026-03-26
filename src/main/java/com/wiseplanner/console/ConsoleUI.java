bf60b44444aa485cb60da55b8ae70670adaa3119
KyrionRan <kyrionran@outlook.com>
2026-03-26 16:23:49 -0400

    Feat: Save user information locally

--- src/main/java/com/wiseplanner/console/ConsoleUI.java
+++ src/main/java/com/wiseplanner/console/ConsoleUI.java
@@ -1,25 +1,56 @@
 package com.wiseplanner.console;
 
+import com.google.gson.Gson;
 import com.wiseplanner.service.Assignment;
+import com.wiseplanner.service.User;
 import com.wiseplanner.service.WisePlannerKernel;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.Scanner;
 
 public class ConsoleUI {
     Scanner scanner = new Scanner(System.in);
+    public static final String DATA_FOLDER = ".wiseplanner";
+    public static final String USER_DATA_FILE = "user.json";
+    WisePlannerKernel wisePlannerKernel;
+    CanvasOutputFormatter canvasOutputFormatter = new CanvasOutputFormatter();
+    TaskOutputFormatter taskOutputFormatter = new TaskOutputFormatter();
+    Gson gson = new Gson();
 
     public void show() throws IOException, URISyntaxException {
-        System.out.println("Please enter your name: ");
-        String name = scanner.nextLine();
-        System.out.println("Please enter your Canvas LMS Access Token");
-        String canvasToken = scanner.nextLine();
-        WisePlannerKernel wisePlannerKernel = new WisePlannerKernel(name, canvasToken);
-        CanvasOutputFormatter canvasOutputFormatter = new CanvasOutputFormatter();
-        TaskOutputFormatter taskOutputFormatter = new TaskOutputFormatter();
 
+        //Initialize User Data
+        String userPath = System.getProperty("user.home");
+        Path userDataPath = Paths.get(userPath, DATA_FOLDER, USER_DATA_FILE);
+        File userDataFile = userDataPath.toFile();
+        User user = null;
+        if (userDataFile.exists()) {
+            try (BufferedReader bufferedReader = new BufferedReader(new FileReader(userDataFile))) {
+                user = gson.fromJson(bufferedReader, User.class);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            System.out.println("Please enter your name: ");
+            String name = scanner.nextLine();
+            System.out.println("Please enter your Canvas LMS Access Token");
+            String canvasToken = scanner.nextLine();
+            user = new User(name, canvasToken);
+            userDataFile.getParentFile().mkdirs();
+            String userJson = gson.toJson(user);
+            try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(userDataFile))) {
+                bufferedWriter.write(userJson);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        wisePlannerKernel = new WisePlannerKernel(user);
+
+        //Console User Interface
         while (true) {
             System.out.println("Main Menu");
             System.out.println("(1) Courses");
