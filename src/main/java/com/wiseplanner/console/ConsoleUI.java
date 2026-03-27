bcf9a6b66d0a4f03e0b0cf3157cab43c745ef3f4
KyrionRan <kyrionran@outlook.com>
2026-03-27 17:57:00 -0400

    Add UserManager Class - Add FileCorruptionException, FileReadException, FileWriteException for exception handling - Change ConsoleUI code

--- src/main/java/com/wiseplanner/console/ConsoleUI.java
+++ src/main/java/com/wiseplanner/console/ConsoleUI.java
@@ -1,54 +1,45 @@
 package com.wiseplanner.console;
 
-import com.google.gson.Gson;
+import com.wiseplanner.exception.FileCorruptionException;
+import com.wiseplanner.exception.FileReadException;
+import com.wiseplanner.exception.FileWriteException;
 import com.wiseplanner.exception.NetworkException;
 import com.wiseplanner.model.Assignment;
-import com.wiseplanner.model.User;
+import com.wiseplanner.service.UserManager;
 import com.wiseplanner.service.WisePlannerKernel;
 
 import java.io.*;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
 import java.util.Scanner;
 
 public class ConsoleUI {
     Scanner scanner = new Scanner(System.in);
-    public static final String DATA_FOLDER = ".wiseplanner";
-    public static final String USER_DATA_FILE = "user.json";
     WisePlannerKernel wisePlannerKernel;
     CanvasOutputFormatter canvasOutputFormatter = new CanvasOutputFormatter();
     TaskOutputFormatter taskOutputFormatter = new TaskOutputFormatter();
-    Gson gson = new Gson();
 
     public void show() {
-
-        //Initialize User Data
-        String userPath = System.getProperty("user.home");
-        Path userDataPath = Paths.get(userPath, DATA_FOLDER, USER_DATA_FILE);
-        File userDataFile = userDataPath.toFile();
-        User user = null;
-        if (userDataFile.exists()) {
-            try (BufferedReader bufferedReader = new BufferedReader(new FileReader(userDataFile))) {
-                user = gson.fromJson(bufferedReader, User.class);
-            } catch (IOException e) {
-                System.err.println("[Error] File read failed, unable to read user data.");
+        UserManager userManager = new UserManager();
+        try {
+            if (userManager.isLogin()) {
+                wisePlannerKernel = new WisePlannerKernel(userManager.getUser());
+            } else {
+                System.out.println("Please enter your name: ");
+                String name = scanner.nextLine();
+                System.out.println("Please enter your Canvas LMS Access Token");
+                String canvasToken = scanner.nextLine();
+                userManager.setUser(name, canvasToken);
+                wisePlannerKernel = new WisePlannerKernel(userManager.getUser());
             }
-        } else {
+        } catch (FileCorruptionException | FileReadException | FileWriteException e) {
+            System.err.println("[Error] " + e.getMessage());
             System.out.println("Please enter your name: ");
             String name = scanner.nextLine();
             System.out.println("Please enter your Canvas LMS Access Token");
             String canvasToken = scanner.nextLine();
-            user = new User(name, canvasToken);
-            userDataFile.getParentFile().mkdirs();
-            String userJson = gson.toJson(user);
-            try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(userDataFile))) {
-                bufferedWriter.write(userJson);
-            } catch (IOException e) {
-                System.err.println("[Error] File write failed, unable to write user data.");
-            }
+            userManager.setUser(name, canvasToken);
+            wisePlannerKernel = new WisePlannerKernel(userManager.getUser());
         }
-        wisePlannerKernel = new WisePlannerKernel(user);
 
         //Console User Interface
         while (true) {
